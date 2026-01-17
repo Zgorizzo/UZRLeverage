25acf64c6837a99cce15891484b11380e32c5930
firafighter <firafighter@zgo.aleeas.com>
2026-01-17 17:44:01 +0100

    config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
       net: false,
       tls: false,
     };
+
+    // Ignore React Native dependencies that MetaMask SDK tries to import
+    // and pino-pretty (dev dependency) when not available
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@react-native-async-storage/async-storage': false,
+      'pino-pretty': false,
+    };
+
+    // Use IgnorePlugin to completely ignore these modules
+    const webpack = require('webpack');
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^@react-native-async-storage\/async-storage$/,
+      }),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^pino-pretty$/,
+      })
+    );
+
     return config;
   },
 };
